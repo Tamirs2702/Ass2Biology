c51d4ad478e52bb9f557b561e74f424164fb5966
Tamir <tamirs2702@gmail.com>
2020-05-21 17:32:17 +0300

    Tamir

---
+++ Image.py
@@ -0,0 +1,15 @@
+# must import if working with opencv in python
+import numpy as np
+import cv2
+
+# removes pixels in image that are between the range of
+# [lower_val,upper_val]
+from matplotlib.pyplot import gray
+
+
+def remove_gray(img,lower_val,upper_val):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_bound = np.array([0,0,lower_val])
+    upper_bound = np.array([255,255,upper_val])
+    mask = cv2.inRange(gray, lower_bound, upper_bound)
+    return cv2.bitwise_and(gray, gray, mask = mask)
